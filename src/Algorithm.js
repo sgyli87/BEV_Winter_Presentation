9dac7a06b4303a6135dc94bc5eb993e95a3bedf3
Yang Li <yangli99@uw.du>
2023-03-03 22:49:25 -0800

    update algo tab

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-function Algorithm() {
+function Algorithm () {
   return (
     <div className="tab-content">
       <h2>Algorithm</h2>
-      <p>This is the Algorithm component.</p>
+      <div className="algo-type">
+        <label>Algorithm: </label>
+        <select id="algo-type-select">
+          <option value="algo1">None</option>
+          <option value="algo2">Algo 1</option>
+        </select>
+      </div>
+      <div className='algo-description'>
+        <label>Description: </label>
+        <text>this is the description </text>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Algorithm;
+export default Algorithm
