86a29ff71d84f54f330b416ec1a29bc1140cc9ea
Yang Li <yangli99@uw.du>
2023-03-13 21:14:42 -0700

    update package.json

--- src/DrivingStyle.js
+++ src/DrivingStyle.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-function DrivingStyle() {
+function DrivingStyle () {
   return (
     <div className="tab-content">
       <h2>Driving Style</h2>
       <p>This is the Driving Style component.</p>
     </div>
-  );
+  )
 }
 
-export default DrivingStyle;
+export default DrivingStyle
