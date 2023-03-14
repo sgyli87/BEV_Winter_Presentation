fd0aa22776f98d77ab8edeae05d029b4a1a78185
Yang Li <yangli99@uw.du>
2023-03-13 19:06:08 -0700

    update route tab

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -1,19 +1,37 @@
 import React from 'react'
 
 function Algorithm () {
+
+  const [selectedAlgo, setAlgo] = useState('None')
+  const [outputAlgo, setOutputAlgo] = useState('Please select an algorithm')
+
+  const handleAlgoChange = (event) => {
+    const value = event.target.value
+    setAlgo(value)
+    if (value === 'Algo 1') {
+      setOutputAlgo(`This is algorithm type: 1`)
+    } else if (value === 'Algo 2') {
+      setOutputAlgo(`This is algorithm type: 2`)
+    } else if (value === 'Algo 3') {
+      setOutputAlgo(`This is algorithm type: 3`)
+    } else {
+      setOutputAlgo('Please select algorithm type first')
+    }
+  }
+
   return (
     <div className="tab-content">
       <h2>Algorithm</h2>
       <div className="algo-type">
         <label>Algorithm: </label>
-        <select id="algo-type-select">
+        <select id="algo-type-select" value={selectedAlgo} onChange={handleAlgoChange}>
           <option value="algo1">None</option>
           <option value="algo2">Algo 1</option>
         </select>
       </div>
       <div className='algo-description'>
         <label>Description: </label>
-        <text>this is the description </text>
+        <text value={outputAlgo} onChange={() => { }}>this is the description </text>
       </div>
     </div>
   )
--- src/Route.js
+++ src/Route.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-function Route() {
+function Route () {
+  const [selectedRoute, setSelectedRoute] = useState('None')
+  const [outputRoute, setOutputRoute] = useState('Please select a body type first')
+
+  const handleRouteChange = (event) => {
+    const value = event.target.value
+    setSelectedRoute(value)
+    if (value === 'Route 2') {
+      setOutputRoute(`Fleet DNA Regional Hall Representative
+Duration: 60min
+Total Distance: 30 miles
+Average Speed:20 mph`)
+    } else if (value === 'Route 3') {
+      setOutputRoute(`Another route
+Duration: 30min
+Total Distance: 25 miles
+Average Speed:40 mph`)
+    } else {
+      setOutputBody('Please select a body type first')
+    }
+  }
+
   return (
     <div className="tab-content">
       <h2>Route</h2>
-      <p>This is the Route component.</p>
+      <div className="Route-type">
+        <label htmlFor="Route-type-select">Route:</label>
+        <select
+          id="Route-type-select"
+          value={selectedRoute}
+          onChange={handleRouteChange}
+        >
+          <option value="Route 1">None</option>
+          <option value="Route 2">Route 1</option>
+          <option value="Route 3">Route 2</option>
+        </select>
+      </div>
+      <div className="output-Route">
+        <h3>Route Info:</h3>
+        <textarea value={outputRoute} onChange={() => { }} style={{ resize: 'none', height: '200px', width: '300px' }} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default Route;
+export default Route
